c0f544a6fbe0e2f21b6eae089fd5940a31a265ed
maengjh <juhee.bda@gmail.com>
2025-09-04 19:15:07 +0900

    [chore] main 수정

--- src/main.py
+++ src/main.py
@@ -1,25 +1,31 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy import select
 
-from src.core.database import get_async_session
-from src.models.models import Users
 from src.routers import users
 
-app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
+def create_app() -> FastAPI:
+    app = FastAPI()
 
-app.include_router(users.router, prefix="/users", tags=["users"])
+    # Static
+    app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
+    # Router
+    app.include_router(users.router, prefix="/users", tags=["users"])
+
+    return app
+
+
+app = create_app()
 
 
 @app.get("/health-check")
 async def health_check():
-    # TODO: DB 조회 테스트
-    async with get_async_session() as session:
-        query = select(Users).where(Users.id == 1)
-        result = await session.execute(query)
-        user = result.scalar()
-        print(user)
+    # # TODO: DB 조회 테스트
+    # async with get_async_session() as session:
+    #     query = select(Users).where(Users.id == 1)
+    #     result = await session.execute(query)
+    #     user = result.scalar()
+    #     print(user)
 
     return {"status": "Success!"}
