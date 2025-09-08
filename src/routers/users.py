5564281f833392f7b012a27a0d829af7efcf6d5f
maengjh <juhee.bda@gmail.com>
2025-09-08 22:16:07 +0900

    [fix] 회원가입 성공시 GET /users/login 으로 리다이렉트

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Form, Request
-from starlette.responses import HTMLResponse
+from fastapi import APIRouter, Form, Query, Request
+from starlette import status
+from starlette.responses import HTMLResponse, RedirectResponse
 
 from src.core.templates import templates
 from src.schemas.users import SignUpRequest
@@ -15,17 +16,31 @@ router = APIRouter()
     description="로그인 및 회원가입 페이지",
     response_class=HTMLResponse,
 )
-async def show_login_page(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(request=request, name="auth.html")
+async def show_login_page(
+    request: Request,
+    message: Annotated[str | None, Query()] = None,
+) -> HTMLResponse:
+    return templates.TemplateResponse(
+        request=request,
+        name="auth.html",
+        context={
+            "message": message,
+        },
+    )
 
 
 @router.post(
     "/signup",
     description="회원 가입",
+    response_class=RedirectResponse,
 )
-async def signup(form_data: Annotated[SignUpRequest, Form()]):
+async def signup(form_data: Annotated[SignUpRequest, Form()]) -> RedirectResponse:
     await user_service.signup(
         email=form_data.email,
         username=form_data.username,
         password=form_data.password,
+    )
+
+    return RedirectResponse(
+        url="/users/login?message=회원가입이 완료되었습니다.\n로그인해주세요.", status_code=status.HTTP_302_FOUND
     )
